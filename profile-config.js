c82361c659cf80f9a668c7413124ba4a76242c68
namkian1 <chalitauttama@gmail.com>
2026-09-07 11:36:36 +0700

    Update profile-config.js

--- profile-config.js
+++ profile-config.js
@@ -1,2 +1,2 @@
 window.STUDENT_PROFILE_WEB_APP_URL =
-  'https://script.google.com/macros/s/AKfycbzZPKCjqrvptWM7nVyquVLeU2GlKrS2UtljX1vQCqDHR2UVsL_-Tyh5BqVctKUwsv1C/exec';
+  'https://script.google.com/macros/s/AKfycbziK43yecq2_zEcG-3ceig--rEStZyCVeElsjaJIqDhOnK3mtDeAU8db7u9H7CdekGH/exec';
